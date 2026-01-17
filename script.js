0da3dd4344d12ffd7c51dfb8fbd5659a88dd3623
yald <97129635@st.deltion.nl>
2026-01-17 10:44:47 +0100

    miniweb.html

--- script.js
+++ script.js
@@ -17,3 +17,75 @@ let item = cart.find(p => p.name === name);
     localStorage.setItem("cart", JSON.stringify(cart));
     location.href = "cart.html";
 }
+
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cartDiv = document.getElementById("cart");
+let total = 0;
+
+if (cart.length === 0) {
+    cartDiv.innerHTML = "<p>Your cart is empty</p>";
+} else {
+    cart.forEach(item => {
+        let quantity = Number(item.quantity) || 1;
+        let price = Number(item.price);
+        let itemTotal = price * quantity;
+
+        total += itemTotal;
+
+        cartDiv.innerHTML += `
+            <div>
+                <img src="${item.image}" alt="${item.name}">
+                <div class="cart-info">
+                    <strong>${item.name}</strong>
+                    <span>Price: $${price.toFixed(2)}</span>
+                    <span>Quantity: 
+                        <button onclick="decreaseQuantity('${item.name}')"<-">-</button>
+                        ${quantity}
+                        <button onclick="increaseQuantity('${item.name}')">+</button>
+                    </span>
+                    
+                    <span>Total: $${itemTotal.toFixed(2)}</span>
+                    <button onclick="removeFromCart('${item.name}')">Remove</button>
+                </div>
+            </div>
+        `;
+    });
+}
+
+document.getElementById("total").innerText =
+    "Grand Total: $" + total.toFixed(2);
+
+    function increaseQuantity(name) {
+        cart = cart.map(item => {
+            if (item.name === name) item.quantity += 1;
+            return item;
+        });
+        localStorage.setItem("cart", JSON.stringify(cart));
+        renderCart();
+    }
+
+    function decreaseQuantity(name) {
+        cart = cart.map(item => {
+            if (item.name === name && item.quantity > 1) item.quantity -= 1;
+            return item;
+        });
+        localStorage.setItem("cart", JSON.stringify(cart));
+        renderCart();
+    }
+
+    function removeFromCart(name) {
+        cart = cart.filter(item => item.name !== name);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        renderCart();
+    
+    }
+
+
+
+    //renderCart();
+
+        function renderCart() {
+            window.location.reload();
+        }
+
